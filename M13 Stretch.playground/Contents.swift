9a667b3fe84da4aa5baa1bba6ea387fcaf57ede8
Tyler Robinson <tyler.robinson.3@gmail.com>
2016-05-26 09:32:46 -0600

    commit Stretch

---
+++ M13 Stretch.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+var numberArray = [12, 34, 2, 76, 45, 34, 51, 3, 90, 101]
+    
+let number = numberArray
+
+func printNumber() -> Int {
+    
+    for i in number <= 50 {
+        print ("low")
+    } else {
+        print ("high")
+    }
+
+    if number / 13 % ==0 {
+        print ("Didnt learn this one in school.")
+    } else {
+        print ("That's easy.")
+    }
+    
+    if number[0] < number[1] {
+    print ("getting bigger")
+    } else {
+        print ("it's stopped growing")
+    }
+}
+
+
